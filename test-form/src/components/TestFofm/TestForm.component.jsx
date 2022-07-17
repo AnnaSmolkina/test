adf98131a0c4d3370d67bcd4a0a65ad2405adecb
Anna Smolkina <annasmolkina1@gmail.com>
2022-07-17 11:24:52 +0300

    add form

--- test-form/src/components/TestFofm/TestForm.component.jsx
+++ test-form/src/components/TestFofm/TestForm.component.jsx
@@ -1,15 +1,131 @@
-import {LeftBlockStyled, TextStyled, WrapperContainer} from "./TestForm.styled";
+import {
+	BackgroundWrapper, CheckNameStyled,
+	LeftBlockStyled,
+	RightBlock,
+	SubtitleStyled,
+	TextStyled,
+	TitleStyled,
+	WrapperContainer
+} from "./TestForm.styled";
 
 
 export const TestFormComponent = () => {
+	const dataFormArray = [
+		{
+			name: 'First Name',
+			type: 'text',
+		},
+		{
+			name: 'Last Name',
+			type: 'text',
+		},
+		{
+			name: 'Nationality',
+			type: 'select',
+			options: [
+				{
+					optionName: 'American',
+					value: 'american'
+				},
+				{
+					optionName: 'Italian',
+					value: 'italian'
+				},
+				{
+					optionName: 'Belarusian',
+					value: 'belarusian'
+				}
+			]
+		},
+		{
+			name: 'E-mail',
+			type: 'text',
+		},
+		{
+			name: 'Date of Birth',
+			type: 'date',
+		},
+		{
+			name: 'Gender',
+			type: 'radio',
+			gender: [
+				{
+					gender: 'Male',
+					value: 'male',
+					name: 'genderName'
+
+				},
+				{
+					gender: 'Female',
+					value: 'female',
+					name: 'genderName'
+				}
+			],
+		},
+		{
+			name: 'Password',
+			type: 'password',
+		},
+		{
+			name: 'Confirm Password',
+			type: 'password',
+		},
+	];
+
 	return (
+		<BackgroundWrapper>
 		<WrapperContainer>
 			<LeftBlockStyled>
 				<TextStyled>
 					Sign up
 				</TextStyled>
 			</LeftBlockStyled>
+			<RightBlock>
+				<TitleStyled>
+					New user?
+				</TitleStyled>
+				<SubtitleStyled>
+					Use the form below to create your account.
+				</SubtitleStyled>
+				<form action="" method="post">
+					{dataFormArray.map((element) => (
+						 element.type === 'text' || element.type === 'password' ? (
+							<div key={element.name}>
+								<label htmlFor={element.name}>{element.name}</label>
+								<input id={element.name} type={element.type}/>
+							</div>
+						) : (
+							 element.type === 'select' ? (
+								 <div>{element.name}
+								 <select>
+									 {element.options.map((nation)=> (
+										 <option key={nation.optionName} value={nation.value}>
+											 {nation.optionName}
+										 </option>
+									 ))}
+								 </select>
+								 </div>
+							 ) : (
+								 element.type === 'radio' ? (
+									 <div > {element.name}
+										 {element.gender.map((gender) => (
+											 <CheckNameStyled key={gender.name}>
+												 <div>
+													 <input id={gender.gender} type={element.type} value={gender.value} name={gender.name}/>
+													 <label htmlFor={gender.gender}>{gender.gender}</label>
+												 </div>
+												 </CheckNameStyled>
+										 ))}
+									 </div>
+								 ) : null
+							 )
+						 )
+
+					))}
+				</form>
+			</RightBlock>
 
 		</WrapperContainer>
+		</BackgroundWrapper>
 	)
 }
--- test-form/src/components/TestFofm/TestForm.styled.js
+++ test-form/src/components/TestFofm/TestForm.styled.js
@@ -1,18 +1,30 @@
 import styled from 'styled-components';
 
+export const BackgroundWrapper = styled.div `
+  max-width: 800px;
+	height: 600px;
+	background: #F2F2F2;
+`;
+
 export const WrapperContainer = styled.div `
   max-width: 665px;
-	max-height: 481px;
+	height: 481px;
+	margin: auto;
+	display: flex;
+	flex-direction: row;
+	border: solid;
 `;
 
 export const LeftBlockStyled = styled.div `
-  max-width: 137.5px;
+	width: 25%;
 	height: 481px;
 	background: #5A61ED;
+//	display: flex;
+//	justify-content: end;
 `;
 
 export const TextStyled = styled.div `
-	height: 100%;
+	min-width:162px;
 	font-family: 'PT Sans',serif;
 	font-style: normal;
 	font-weight: 400;
@@ -21,4 +33,32 @@ export const TextStyled = styled.div `
 	letter-spacing: 0.5px;
 	color: #FFFFFF;
 	transform: rotate(-90deg);
+`;
+
+export const RightBlock = styled.div `
+  width: 75%;
+	height: 481px;
+	background: white;
+`;
+
+export const TitleStyled = styled.div `
+	font-family: 'WorkSans',serif;
+	font-size: 22px;
+	line-height: 26px;
+	letter-spacing: -0.46px;
+	color: #111111;
+`;
+
+export const SubtitleStyled = styled.div `
+	font-family: 'WorkSans',serif;
+	font-size: 22px;
+	line-height: 26px;
+	letter-spacing: -0.46px;
+	color: #111111;
+`;
+
+export  const CheckNameStyled = styled.div`
+display: flex;
+	flex-direction: row;
+	justify-content: space-around;
 `;
